b00756227f4a1597c66025dc662375e7461432be
Joe_chen <joe824111@gmail.com>
2020-11-10 21:27:54 +0800

    刪除與修改

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ new Vue({
         inputText: "",
         list: [],
         show: "all",
+        compositionStatus:false,
+        editing:null,
+        editingText: "",
     },
     computed: {
         filterList() {
@@ -17,13 +20,42 @@ new Vue({
         }
     },
     methods: {
+        cstartHandler(){
+            this.compositionStatus = true
+        },
+        cendHandler(){
+            this.compositionStatus = false
+        },
         inputHandler() {
+            if (this.compositionStatus) return false
             this.list.push({
                 timestamp: new Date().getTime(),
                 status: false,
                 content: this.inputText
             })
             this.inputText = ""
+        },
+        deleteHandler(index){
+            // 1. 第一種
+            // this.list = this.list.filter((item,i)=>{
+            //     return i != index
+            // })
+
+            // 2. 第二種
+            this.list.splice(index,1)
+        },
+        editHandler(item){
+            this.editing = item
+            this.editingText = item.content
+        },
+        completeHandler(){
+            this.editing.content= this.editingText
+            this.cancelHandler()
+        },
+        cancelHandler(){
+            this.editingText = ""
+            this.editing = null
         }
+
     }
 })
