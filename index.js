81e5966c7a2ad400a9e7db11cc34638928a1522e
Joe_chen <joe824111@gmail.com>
2020-11-10 20:27:09 +0800

    顯示輸入框資料

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 new Vue({
-    el:"#app",
-    data:{
-        inputText:"",
-        list:[],
+    el: "#app",
+    data: {
+        inputText: "",
+        list: [],
+        show: "all",
     },
-    computed:{
-
+    computed: {
+        filterList() {
+            if (this.show === "all") {
+                return this.list
+            } else if (this.show === "open") {
+                return this.list.filter((item) => { return item.status === false })
+            } else if (this.show === "done") {
+                return this.list.filter((item) => { return item.status === true })
+            }
+        }
     },
-    methods:{
-        inputHandler(){
+    methods: {
+        inputHandler() {
             this.list.push({
                 timestamp: new Date().getTime(),
                 status: false,
